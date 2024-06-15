b575cde616ab1a7e89b74be23c935ce316eb8cc1
mistadrumma <breakroger104@gmail.com>
2024-06-15 10:33:33 +0300

    pnpx prisma migrate deploy

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from 'src/prisma.service'
+import { PrismaService } from 'src/prisma/prisma.service'
 import { UserController } from './user.controller'
 import { UserService } from './user.service'
 
